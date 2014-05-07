6bdbaec6be8722dc884db3b0a86971b3d4f21a67
zcfrank1st <zhchaos@gmail.com>
2014-05-07 16:19:37 +0800

    添加hive thrift查询相关内容
    待测试

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 var server = require('connect');
+var hive = require('./service/hive');
 
-function test (req, res, next) {
-  res.end("hello world");
+// config
+var port = 3000;
+var ip = '';
+
+var hive_Cli = hive.HiveCli();
+
+function logger (req, res, next) {
+  // TODO 初始化log
   next();
 }
 
+function query_hive (req, res, next) {
+  // TODO 从req当中获取 查询sql
+  var sql = '';
+  
+  var hivecli = new hive();
+  hivecli.query(sql);
+  next();
+}
 
 // TODO 实现静态服务器是基本功能，同时通过编写插件可以调用hive
 server()
-  .use(test)
-  .listen(3000, function () {
-    console.log("chaoserver is running~");
+  .use(logger)
+  .use('/hive/exec', query_hive)
+  .listen(port, function () {
+    console.log("chaoserver is running at port " + port);
   });
--- service/hive.js
+++ service/hive.js
@@ -1,7 +1,42 @@
-var thrift = require('thrift');
-var ThriftTransports = require('thrift/transport');
-var ThriftProtocols = require('thrift/protocol');
+var EventEmitter = require('events').EventEmitter;
+var util = require("util"); 
+
+var TTransports = require('thrift/transport');
+var TProtocols = require('thrift/protocol');
 var TCLIService = require('../thrift/gen-nodejs/TCLIService.js');
-var ttypes = require('../thrift/gen-nodejs/TCLIService_types.js');
 
+// config
+var host = 'localhost';
+var port = '';
 
+// thrift init
+var socket = TTransports.Tsocket(host, port);
+var transport = TTransports.TBufferedTransport(socket);
+var protocol = TProtocols.TBinaryProtocol(transport);
+var client = TCLIService.Client(protocol);
+transport.open();
+
+// hive类
+function Hive() {
+}
+util.inherits(Hive, EventEmitter);
+
+Hive.prototype.query = function (sql) {
+  var self = this;
+  
+  self.on('done', function () {
+    // TODO 调用某些方法进行数据的处理
+  });
+  
+  client.ExecuteStatement(sql, function (err, res) {
+    if (err) {
+      // TODO 处理错误
+    } else {
+      self.emit('done', res);
+    }
+  });
+};
+
+// TODO hive method
+
+module.exports = Hive;
