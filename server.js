4436e8940bd1fedb5a29c53c482694df3ea45b1e
zcfrank1st <zhchaos@gmail.com>
2014-05-08 20:57:19 +0800

    增加.gitignore规则，server打日志基本实现
    增加.gitignore规则，server打日志基本实现

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var server = require('connect');
 var logger = require('morgan');
+var fs = require('fs');
 var CONSTANTS = require('./constants');
 var self_modules = require('./modules');
 
 server()
+.use(logger({stream: fs.createWriteStream('./server.log', {'flags': 'a'})}))
 .use(logger('dev'))
 .use(self_modules.test)
 .listen(CONSTANTS.port, function () {
