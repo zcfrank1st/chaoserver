56aac97f1152afb93501f57689d0c899470888a8
zcfrank1st <zhchaos@gmail.com>
2014-05-08 14:50:03 +0800

    add skeleton
    add server skeleton and remove thift content

---
+++ modules.js
@@ -0,0 +1,5 @@
+var self_modules = module.exports;
+
+self_modules.test = function (req, res, next) {
+  console.log("hello");
+}
--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 var server = require('connect');
-var hive = require('./service/hive');
+var constants = require('./constants');
+var self_modules = require('./modules');
 
-// config
-var port = 3000;
-var ip = '';
-
-var hive_Cli = hive.HiveCli();
-
-function logger (req, res, next) {
-  // TODO 初始化log
-  next();
-}
-
-function query_hive (req, res, next) {
-  // TODO 从req当中获取 查询sql
-  var sql = '';
-  
-  var hivecli = new hive();
-  hivecli.query(sql);
-  next();
-}
-
-// TODO 实现静态服务器是基本功能，同时通过编写插件可以调用hive
 server()
-  .use(logger)
-  .use('/hive/exec', query_hive)
-  .listen(port, function () {
-    console.log("chaoserver is running at port " + port);
-  });
+.use(self_modules.test)
+.listen(constants.port, function () {
+  console.log("server is running at port " + constants.port);
+});
