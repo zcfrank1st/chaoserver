60a32492ac85c7ec0ef140d7377d5acc3026b570
zcfrank1st <zhchaos@gmail.com>
2014-05-09 11:19:41 +0800

    实现页面请求容错

--- modules.js
+++ modules.js
@@ -1,5 +1,22 @@
+var fs = require('fs');
+var CONSTANTS = require('./constants');
+var service = require('./services');
+
 var self_modules = module.exports;
 
-self_modules.test = function (req, res, next) {
-  res.end('hello world');
-}
+self_modules.test = function (req, res) {
+  console.log(req.method);
+  res.end();
+};
+
+self_modules.test2 = function (req, res) {
+  console.log(req);
+  res.end();
+};
+
+self_modules.defultRoute = function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFile(CONSTANTS.webroot + 'index.html', function (err, data) {
+    res.end(data);
+  });
+};
--- server.js
+++ server.js
@@ -9,13 +9,17 @@ var self_modules = require('./modules');
 
 var host = CONSTANTS.ip || '0.0.0.0';
 
-
-// TODO 增加方法请求处理
 server()
+// init
 .use(logger({stream: fs.createWriteStream('./server.log', {'flags': 'a'})}))
 .use(logger('dev'))
 .use(serveStatic(CONSTANTS.webroot, {'index':['index.html']}))
+// Restful
 .use('/me', self_modules.test)
+.use('/me2', self_modules.test2)
+// default page
+.use(self_modules.defultRoute)
+// listen port and ip
 .listen(CONSTANTS.port, host, function () {
   console.log("server is running at port " + CONSTANTS.port);
 });
